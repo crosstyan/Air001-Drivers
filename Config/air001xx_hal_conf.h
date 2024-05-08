5a93cdf50776b7190f79774f6edf3246fe4f62cf
crosstyan <crosstyan@outlook.com>
2024-05-08 11:31:32 +0800

    move CMakeLists inside

---
+++ Config/air001xx_hal_conf.h
@@ -0,0 +1,10 @@
+#ifndef __AIR001xx_HAL_CONF_H
+#define __AIR001xx_HAL_CONF_H
+
+#define HAL_MODULE_ENABLED 1
+#define HAL_CORTEX_MODULE_ENABLED 1
+#define HAL_GPIO_MODULE_ENABLED 1
+#define HAL_RCC_MODULE_ENABLED 1
+
+#include "air001xx_hal_conf_default.h"
+#endif /* __AIR001xx_HAL_CONF_H */
